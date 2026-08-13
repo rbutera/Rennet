77490ab4f43aa22d5a22fcf3f39552d931bb4c28
Rai Butera <rai@rbutera.com>
2026-08-13 09:44:54 +0100

    Build Rennet marketing site

---
+++ apps/marketing/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  site: "https://rennet.dev",
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
+});
